a2029fe8a136a0e112475e7d3deb6f3da3a5f779
JP <jpmezzara@me.com>
2020-03-21 22:17:14 -0600

    Product composite service

---
+++ src/test/java/se/magnus/compositeservice/ProductCompositeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.magnus.compositeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCompositeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
